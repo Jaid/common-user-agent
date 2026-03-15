02ee8ea6c9f5411b9ec8d66d449d2cc2642ae4e1
Jaid <6216144+Jaid@users.noreply.github.com>
2026-03-15 18:27:24 +0100

    Added user agent
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,8 @@
-import { it, expect } from "bun:test"
+import {expect, it} from 'bun:test'
 
 import {getMainModuleDefault} from 'zeug'
 
-const commonUserAgent = await getMainModuleDefault<typeof import('../src/index.ts')>('common-user-agent')
-
-it("should run", () => {
-  expect(commonUserAgent).toBe(1) // TODO Test actual functionality
+const commonUserAgent = await getMainModuleDefault<typeof import('../src/main.ts')>('src/main.ts')
+it('should run', () => {
+  expect(commonUserAgent).toMatch(/^Mozilla\/\d+\.0 /)
 })
