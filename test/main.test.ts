f805fb76163c54cb41ed093d70b5a41ac63a50e4
Jaid <6216144+Jaid@users.noreply.github.com>
2026-03-15 22:47:48 +0100

    Extended test
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- test/main.test.ts
+++ test/main.test.ts
@@ -4,5 +4,8 @@ import {getMainModuleDefault} from 'zeug'
 
 const commonUserAgent = await getMainModuleDefault<typeof import('../src/main.ts')>('src/main.ts')
 it('should run', () => {
-  expect(commonUserAgent).toMatch(/^Mozilla\/\d+\.0 /)
+  expect(commonUserAgent).toMatch(/(^| )Mozilla\/\d+/)
+  expect(commonUserAgent).toMatch(/(^| )AppleWebKit\/\d+/)
+  expect(commonUserAgent).toMatch(/(^| )Chrome\/\d+/)
+  expect(commonUserAgent).toMatch(/(^| )Safari\/\d+/)
 })
