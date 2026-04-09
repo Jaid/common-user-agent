501bd188c3f3651b85184b2a805b001dedc276e3
Jaid <6216144+Jaid@users.noreply.github.com>
2026-04-09 12:42:05 +0100

    Fixed string building
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import versions from './versions.yml'
 
 const webkitVersion = `${versions.webkit.major}.${versions.webkit.minor}`
-const chromeVersion = `${versions.chrome.major}.${versions.chrome.minor}`
+const chromeVersion = `${versions.chrome.major}.${versions.chrome.minor}.${versions.chrome.build}.${versions.chrome.patch}`
 const mozillaVersion = `${versions.mozilla.major}.${versions.mozilla.minor}`
-export default `Mozilla/${mozillaVersion} (Windows NT 10.0; Win64; x64) AppleWebKit/${webkitVersion} (KHTML, like Gecko) Chrome/${chromeVersion}.0.0.0 Safari/${webkitVersion}`
+export default `Mozilla/${mozillaVersion} (Windows NT 10.0; Win64; x64) AppleWebKit/${webkitVersion} (KHTML, like Gecko) Chrome/${chromeVersion} Safari/${webkitVersion}`
