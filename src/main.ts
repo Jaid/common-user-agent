1d0b8dd520897335e3bb89d37c697933a37a9125
Jaid <6216144+Jaid@users.noreply.github.com>
2026-04-09 13:31:11 +0100

    Fixed Chrome version being too specific
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import versions from './versions.yml'
 
 const webkitVersion = `${versions.webkit.major}.${versions.webkit.minor}`
-const chromeVersion = `${versions.chrome.major}.${versions.chrome.minor}.${versions.chrome.build}.${versions.chrome.patch}`
+const chromeVersion = `${versions.chrome.major}.0.0.0`
 const mozillaVersion = `${versions.mozilla.major}.${versions.mozilla.minor}`
 export default `Mozilla/${mozillaVersion} (Windows NT 10.0; Win64; x64) AppleWebKit/${webkitVersion} (KHTML, like Gecko) Chrome/${chromeVersion} Safari/${webkitVersion}`
